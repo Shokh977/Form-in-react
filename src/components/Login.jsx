b0ffab6e3ac759a83816be5593a6084d7fad03de
Shokhrukh <uphill7165@gmail.com>
2024-01-29 14:13:50 +0900

    added useState hook to get values of the inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,51 @@
 import { useState } from "react";
 
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(enteredValues);
+  };
+
+  const handleChangeInput = (identifier, value) => {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      // square brackets are used to wrap 'identifier'. This syntax is part of ES6 and allows "dynamically" set the property name of an object using a variable.
+      [identifier]: value,
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleChangeInput("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleChangeInput("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
